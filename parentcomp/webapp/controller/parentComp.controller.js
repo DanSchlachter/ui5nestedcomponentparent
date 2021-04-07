c4908ca5b054b43d5807671a82061d75c696583e
Daniel Schlachter <schlachter.daniel@gmail.com>
2021-04-07 13:04:07 +0000

    connect parent/child WIP

--- parentcomp/webapp/controller/parentComp.controller.js
+++ parentcomp/webapp/controller/parentComp.controller.js
@@ -8,8 +8,20 @@ sap.ui.define([
 		"use strict";
 
 		return Controller.extend("ns.parentComp.parentcomp.controller.parentComp", {
-			onInit: function () {
-
+			 onInit: function () {
+	        // // get the child container
+			// var oChildContainer = this.byId("myChildContainer");
+			// this.getOwnerComponent().runAsOwner(function () {
+			// 	sap.ui.component({
+			// 		name: "ns.child.childcomp", //Our child component
+			// 		id: "childcomp",
+			// 		manifestFirst: true,
+			// 		async: true
+			// 	}).then(function (component) {
+			// 		// set the component when it is successfully loaded
+			// 		oChildContainer.setComponent(component);
+			// 	}.bind(this));
+			// }.bind(this));
 			}
 		});
 	});
